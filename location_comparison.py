e6f9a39bf6075c3c21288f44391f892e5aca63b6
mdshiozaki <michaels710@live.com>
2020-03-07 14:00:44 -0500

    Removed unnecessary abs and xs

--- location_comparison.py
+++ location_comparison.py
@@ -3,8 +3,8 @@ import math
 def compare(x_shot, y_shot, x_target, y_target, px_to_cm):
 
     #ratio: 3:2
-    x_diff = abs(x_target - x_shot)
-    y_diff = abs(y_target - y_shot)xxxx
+    x_diff = x_target - x_shot
+    y_diff = y_target - y_shot
 
     diff_length_px = math.sqrt(x_diff**2 + y_diff**2)
     diff_length_cm = diff_length_px * px_to_cm
